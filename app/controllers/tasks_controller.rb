c2464ec5dcf62d7893c788229236dbb4b9594b04
Grant Schroeder <gschroeder@pznma.com>
2019-09-05 20:51:42 +0000

    Task update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,23 @@
 class TasksController < ApplicationController
     def index
+      # list all Task that are stored
         render json: Task.all
+      end
+    
+      # method for updating task completed or not
+      def update
+        # call for specific task id 
+        task = Task.find(params[:id])
+        # checks update is in the task prarams
+        task.update_attributes(task_params)
+        # updates id task on page
+        render json: task
+      end
+    
+      private
+    # method for defining requirments of task
+      def task_params
+    # specified requirments
+        params.require(:task).permit(:done)
       end
 end
